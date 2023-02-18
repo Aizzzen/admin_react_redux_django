18ecbf0114813cb01481089426b370b7a6e247da
Yunus <gadamurrr@gmail.com>
2023-02-18 16:41:22 +0300

    webpack&react_frontend

--- admin_panel/admin_panel/urls.py
+++ admin_panel/admin_panel/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from leads.views import LeadViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
+    path('', include('leads.urls')),
 ]
